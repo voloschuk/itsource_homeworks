98306fe93ec6ab4460d6d52209f75b970552caf6
voloschukA <voloschuk.alla@gmail.com>
2017-05-15 23:37:56 +0300

    homework 3. binary search.

---
+++ src/com/sourceit/com/sourceit/lesson3/InsertSort.java
@@ -0,0 +1,13 @@
+package com.sourceit.com.sourceit.lesson3;
+
+import java.util.Arrays;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] array = {5, 7, 9, 15, 10, -1, 8};
+
+        /*Your code here*/
+
+        System.out.println(Arrays.toString(array));
+    }
+}
