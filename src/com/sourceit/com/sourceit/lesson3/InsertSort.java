1fb18ce56fd8da4cd80af9b02783fe736b925b96
voloschukA <voloschuk.alla@gmail.com>
2017-05-18 01:11:58 +0300

    homework 3. matrix printer.

--- src/com/sourceit/com/sourceit/lesson3/InsertSort.java
+++ src/com/sourceit/com/sourceit/lesson3/InsertSort.java
@@ -6,7 +6,20 @@ public class InsertSort {
     public static void main(String[] args) {
         int[] array = {5, 7, 9, 15, 10, -1, 8};
 
-        /*Your code here*/
+        for (int upIndex = 1; upIndex < array.length; upIndex++) {
+            int elem = array[upIndex];
+            if (array[upIndex - 1] > elem) {
+                int insIndex = 0;
+                for (int downIndex = upIndex - 1; downIndex >= 0; downIndex--) {
+                    if (array[downIndex] <= elem) {
+                        insIndex = downIndex + 1;
+                        break;
+                    }
+                }
+                System.arraycopy(array, insIndex, array, insIndex + 1, upIndex - insIndex);
+                array[insIndex] = elem;
+            }
+        }
 
         System.out.println(Arrays.toString(array));
     }
